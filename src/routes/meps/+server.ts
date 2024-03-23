7cb709b5e2aedf4078c99ce6ac82f72c5187ea73
Antonio Curado <abestevao@gmail.com>
2024-03-23 19:21:36 +0100

    Update eu-parliment-votes-sdk version to 1.9.1

--- src/routes/meps/+server.ts
+++ src/routes/meps/+server.ts
@@ -1,17 +1,17 @@
-// // SvelteKit GET function that returns a single vote
-// import type { RequestHandler } from "./$types";
-// import { loadMeps } from "eu-parliment-votes-sdk";
+// SvelteKit GET function that returns a single vote
+import type { RequestHandler } from "./$types";
+import { loadMeps } from "eu-parliment-votes-sdk";
 
-// export const GET: RequestHandler = async ({ url, platform }) => {
-//   let votes = await loadMeps();
+export const GET: RequestHandler = async ({ url, platform }) => {
+  let votes = await loadMeps();
 
-//   if (votes === null) {
-//     return new Response(JSON.stringify({}), {
-//       headers: { "Content-Type": "application/json" },
-//       status: 500,
-//     });
-//   }
-//   return new Response(JSON.stringify(votes), {
-//     headers: { "Content-Type": "application/json" },
-//   });
-// };
+  if (votes === null) {
+    return new Response(JSON.stringify({}), {
+      headers: { "Content-Type": "application/json" },
+      status: 500,
+    });
+  }
+  return new Response(JSON.stringify(votes), {
+    headers: { "Content-Type": "application/json" },
+  });
+};

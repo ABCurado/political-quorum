ca8c5dc6e02edfd7a1555454f02a851c2153e5e1
Antonio Curado <abestevao@gmail.com>
2024-03-23 21:39:39 +0100

    Remove enhancedImages plugin and external dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [enhancedImages(),sveltekit()],
-	build: {
-		rollupOptions: {
-			external: ["eu-parliment-votes-sdk"]
-		}
-	}
+	plugins: [sveltekit()]
 });
